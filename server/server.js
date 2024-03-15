45e74869d1cebd3e465b5574f3d78fbe8fd1efe5
praveen <kpking900@gmail.com>
2024-03-15 17:24:54 +0530

    by praveen

--- server/server.js
+++ server/server.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const path = require("path");
 
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
+
+const _dirname = path.dirname("");
+const buildpath = path.join(_dirname, "../client/build");
+app.use(express.static(buildpath));
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 module.exports = app;
